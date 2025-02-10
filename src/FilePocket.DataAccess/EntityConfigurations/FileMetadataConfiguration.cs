ed187c82264adec058fdbd31c2bf1a5845f7b373
Viacheslav Myronenko <feynmanity@gmail.com>
2025-02-10 22:56:41 +0100

    FP-00006: Reworked FileMetadata, FileMetadataConfiguration

--- src/FilePocket.DataAccess/EntityConfigurations/FileMetadataConfiguration.cs
+++ src/FilePocket.DataAccess/EntityConfigurations/FileMetadataConfiguration.cs
@@ -6,7 +6,41 @@ namespace FilePocket.DataAccess.EntityConfigurations;
 
 public class FileMetadataConfiguration : IEntityTypeConfiguration<FileMetadata>
 {
+    private const int MaxActualNameLength = 255;
+    private const int MaxOriginalNameLength = 255;
+
+    // Not sure, but need to set the max path length to avoid varchar(max) in db level
+    public const int MaxPathLength = 255;
+
     public void Configure(EntityTypeBuilder<FileMetadata> builder)
     {
+        builder.HasIndex(f => f.ActualName)
+            .IsUnique();
+
+        builder.Property(f => f.ActualName)
+            .HasMaxLength(MaxActualNameLength)
+            .IsRequired();
+
+        builder.Property(f => f.OriginalName)
+            .HasMaxLength(MaxOriginalNameLength)
+            .IsRequired();
+
+        builder.Property(f => f.Path)
+            .IsRequired();
+
+        builder.Property(f => f.FileType)
+            .IsRequired();
+
+        builder.Property(f => f.DateCreated)
+            .IsRequired();
+
+        builder.Property(f => f.FileSize)
+            .IsRequired();
+
+        builder.Property(f => f.UserId)
+            .IsRequired();
+
+        builder.Property(f => f.IsDeleted)
+            .HasDefaultValue(false);
     }
 }
