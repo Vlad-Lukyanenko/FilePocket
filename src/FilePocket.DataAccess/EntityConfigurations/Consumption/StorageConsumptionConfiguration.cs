5a347573353e282c9ac832959f9debe898e9459e
Viacheslav Myronenko <feynmanity@gmail.com>
2025-02-10 23:05:47 +0100

    FP-00006: Added Account/Storage configurations

---
+++ src/FilePocket.DataAccess/EntityConfigurations/Consumption/StorageConsumptionConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using FilePocket.DataAccess.Extensions;
+using FilePocket.Domain.Entities.Consumption;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FilePocket.DataAccess.EntityConfigurations.Consumption;
+
+public class StorageConsumptionConfiguration : IEntityTypeConfiguration<StorageConsumption>
+{
+    public void Configure(EntityTypeBuilder<StorageConsumption> builder)
+    {
+        builder.HasBaseType<AccountConsumption>();
+
+        builder.Property(sc => sc.Used)
+            .HasDefaultPrecision()
+            .HasDefaultValue(0)
+            .IsRequired();
+
+        builder.Property(sc => sc.Total)
+            .HasDefaultPrecision()
+            .IsRequired();
+    }
+}
