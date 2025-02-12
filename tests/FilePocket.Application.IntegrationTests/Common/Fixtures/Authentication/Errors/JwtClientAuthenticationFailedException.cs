de9c48be0af95fd09726c5fbd8f1e6f836ecf62e
Viacheslav Myronenko <feynmanity@gmail.com>
2025-02-12 15:44:06 +0100

    FP-00006: Added ClientAuthenticationFixture to handle user sign-in/sign-up flows

---
+++ tests/FilePocket.Application.IntegrationTests/Common/Fixtures/Authentication/Errors/JwtClientAuthenticationFailedException.cs
@@ -0,0 +1,3 @@
+﻿namespace FilePocket.Application.IntegrationTests.Common.Fixtures.Authentication.Errors;
+
+public sealed class JwtClientAuthenticationFailedException(string context) : Exception($"Jwt client authentication failed. Context: {context}");
