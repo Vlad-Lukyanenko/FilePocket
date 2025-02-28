deb57e459d413b49b18d6cffa28d4c0751ec4763
dp <fandorine77@gmail.com>
2025-02-28 22:12:40 +0200

    Display all user's bookmarks.

--- src/FilePocket.BlazorClient/Pages/Bookmarks/Bookmarks.razor.cs
+++ src/FilePocket.BlazorClient/Pages/Bookmarks/Bookmarks.razor.cs
@@ -1,17 +1,27 @@
-﻿using FilePocket.BlazorClient.Services.Pockets.Requests;
+﻿using FilePocket.BlazorClient.Features.Bookmarks.Models;
+using FilePocket.BlazorClient.Features.Bookmarks.Requests;
+using FilePocket.BlazorClient.Services.Pockets.Requests;
 using Microsoft.AspNetCore.Components;
 
 namespace FilePocket.BlazorClient.Pages.Bookmarks;
 
 public partial class Bookmarks
 {
+    private List<BookmarkModel> _bookmarks = new List<BookmarkModel>();
+    private bool _loading = true;
+    private Guid _bookmarkIdToBeDeleted;
+
     [Parameter] public Guid PocketId { get; set; }
     [Parameter] public Guid? FolderId { get; set; }
 
     [Inject] private IPocketRequests PocketRequests { get; set; } = default!;
+    [Inject] private IBookmarkRequests BookmarkRequests { get; set; } = default!;
 
     protected override async Task OnInitializedAsync()
     {
+        _bookmarks = (await BookmarkRequests.GetAllAsync()).ToList();
+        _loading = false;
+
         if (PocketId == Guid.Empty)
         {
             var defaultPocket = await PocketRequests.GetDefaultAsync();
