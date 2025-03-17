8931ccb46836afa6cad9cc5b4c3f1667b5de91d4
dp <fandorine77@gmail.com>
2025-03-17 09:39:47 +0200

    Added 'Profile' entity to database.

---
+++ src/FilePocket.Domain/Entities/Profile.cs
@@ -0,0 +1,22 @@
+﻿namespace FilePocket.Domain.Entities;
+
+public class Profile
+{
+    public Guid Id { get; init; }
+
+    public string? FirstName { get; set; }
+
+    public string? LastName { get; set; }
+
+    public string? Email { get; set; } = string.Empty;
+
+    public Guid? IconId { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+
+    public Guid UserId { get; set; }
+
+    public virtual User User { get; set; } = null!;
+}
