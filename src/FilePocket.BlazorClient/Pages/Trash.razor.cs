b7281a3c83950b284566520e729667e98292df1e
dp <fandorine77@gmail.com>
2025-04-01 11:49:49 +0300

    Implemented Trash component.

---
+++ src/FilePocket.BlazorClient/Pages/Trash.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace FilePocket.BlazorClient.Pages;
+
+public partial class Trash
+{
+    [Inject] private NavigationManager Navigation { get; set; } = default!;
+
+    private void Navigate(string uri)
+    {
+        Navigation.NavigateTo(uri);
+    }
+}
