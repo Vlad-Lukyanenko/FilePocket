18f1d8d57313ea0f2f6868af726f2c10df67296e
MichaelFadyeyev <mihail.fadeev@ukr.net>
2025-05-01 17:31:28 +0300

    Add Foldertype.Note; Fix Issue with page redirection on folder double click; remove unnecessary AutoMapper

--- src/FilePocket.BlazorClient/MyComponents/NotesAndFolders.razor.cs
+++ src/FilePocket.BlazorClient/MyComponents/NotesAndFolders.razor.cs
@@ -83,7 +83,7 @@ namespace FilePocket.BlazorClient.MyComponents
                 return;
             }
 
-            var folderTypes = new List<FolderType> { FolderType.Documents };
+            var folderTypes = new List<FolderType> { FolderType.Notes };
             _currentFolder = FolderId is null ? null : await FolderRequests.GetAsync(PocketId!.Value, FolderId.Value);
 
             var folders =
