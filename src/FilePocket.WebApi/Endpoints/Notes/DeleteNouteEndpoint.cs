07a0c4ae80d0160d1f9d36e2f35d136a9ba7525d
MichaelFadyeyev <mihail.fadeev@ukr.net>
2025-05-07 23:39:37 +0300

    Add CRUD Endpoints for Notes

---
+++ src/FilePocket.WebApi/Endpoints/Notes/DeleteNouteEndpoint.cs
@@ -0,0 +1,32 @@
+﻿using FilePocket.Application.Interfaces.Services;
+using FilePocket.WebApi.Endpoints.Base;
+
+namespace FilePocket.WebApi.Endpoints.Notes
+{
+    public class DeleteNoteEndpoint : BaseEndpointWithoutRequestAndResponse
+    {
+        private readonly IServiceManager _service;
+        public DeleteNoteEndpoint(IServiceManager service)
+        {
+            _service = service;
+        }
+        public override void Configure()
+        {
+            Delete("api/notes/{id:guid}/delete-irreversibly");
+            AuthSchemes("Bearer");
+        }
+        public override async Task HandleAsync(CancellationToken cancellationToken)
+        {
+            var id = Route<Guid>("id");
+            var result = await _service.FileService.RemoveFileAsync(UserId, id, cancellationToken);
+
+            if (result)
+            {
+                await SendOkAsync(cancellationToken);
+                return;
+            }
+
+            await SendNotFoundAsync(cancellationToken);
+        }
+    }
+}
