55822596b891c940d76889322f96eb84ebde5f79
dp <fandorine77@gmail.com>
2025-05-20 12:36:34 +0300

    Clear recycle bin from UI.

--- src/FilePocket.BlazorClient/Pages/Trash.razor.cs
+++ src/FilePocket.BlazorClient/Pages/Trash.razor.cs
@@ -1,10 +1,21 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using FilePocket.BlazorClient.Features.Trash;
+using Microsoft.AspNetCore.Components;
 
 namespace FilePocket.BlazorClient.Pages;
 
 public partial class Trash
 {
+    private bool _clearTrashStarted;
+
     [Inject] private NavigationManager Navigation { get; set; } = default!;
+    [Inject] private ITrashRequests TrashRequests { get; set; } = default!;
+
+    private async Task ClearTrashClickAsync()
+    {
+        await TrashRequests.ClearAllTrashAsync();
+
+        Navigation.NavigateTo("/");
+    }
 
     private void Navigate(string uri)
     {
